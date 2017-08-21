2196490a09eac7f12b75a8eb338fc5fe127794a9
tutr-PC\tutrieuchau <nguyenductamwind@gmail.com>
2017-08-21 08:40:03 +0700

    Create Android Project

---
+++ WinWin/app/src/main/java/com/tutrieuchau/winwin/Activity/TimeActivity.java
@@ -0,0 +1,15 @@
+package com.tutrieuchau.winwin.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tutrieuchau.winwin.R;
+
+public class TimeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_time);
+    }
+}

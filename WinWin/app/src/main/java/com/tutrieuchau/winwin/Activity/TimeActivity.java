791b6d1e19023c1bf6c8de5efd4a6e6201e2a1ff
CUBEVN\ductam <ductam@vn-cubesystem.com>
2017-08-29 16:38:35 +0700

    Update Chart

--- WinWin/app/src/main/java/com/tutrieuchau/winwin/Activity/TimeActivity.java
+++ WinWin/app/src/main/java/com/tutrieuchau/winwin/Activity/TimeActivity.java
@@ -1,15 +1,127 @@
 package com.tutrieuchau.winwin.Activity;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.tutrieuchau.winwin.R;
 
-public class TimeActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class TimeActivity extends AppCompatActivity {
+    private  PieChart pieChart;
+    private RelativeLayout mainPieChartLayout;
+    private float[] ydata = {5,10,15,30,40};
+    private String[] xdata = {"Android Studio","Xcode","Visual Studio","Notepad++","Edit Text"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_time);
+        mainPieChartLayout = (RelativeLayout) findViewById(R.id.mainPieChart);
+        pieChart = new PieChart(this);
+        mainPieChartLayout.addView(pieChart);
+        mainPieChartLayout.setBackgroundColor(Color.LTGRAY);
+
+        pieChart.setUsePercentValues(true);
+        Description description = new Description();
+        description.setText("Code Editor");
+        pieChart.setDescription(description);
+
+        pieChart.setDrawHoleEnabled(true);
+        pieChart.setHoleRadius(7);
+        pieChart.setTransparentCircleRadius(10);
+
+        pieChart.setRotationAngle(0);
+        pieChart.setRotationEnabled(true);
+
+        pieChart.setOnChartValueSelectedListener(new OnChartValueSelectedListener() {
+            @Override
+            public void onValueSelected(Entry e, Highlight h) {
+
+            }
+
+            @Override
+            public void onNothingSelected() {
+
+            }
+        });
+        //add data
+        addDataToPieChart();
+
+        //
+        Legend le = pieChart.getLegend();
+        le.setPosition(Legend.LegendPosition.RIGHT_OF_CHART);
+        le.setXEntrySpace(7);
+        le.setYEntrySpace(5);
+
+
+
+    }
+    private void addDataToPieChart(){
+        List<PieEntry> yVals = new ArrayList<>();
+        for (int i = 0; i < ydata.length ; i ++){
+            yVals.add(new PieEntry(ydata[i],i));
+
+        }
+        ArrayList<String> xVals = new ArrayList<>();
+
+        for(int i = 0; i < xdata.length ; i++){
+            xVals.add(xdata[i]);
+
+        }
+        PieDataSet pieDataSet = new PieDataSet(yVals,"Code edit");
+        pieDataSet.setSliceSpace(3);
+        pieDataSet.setSelectionShift(5);
+
+        ArrayList<Integer> colors = new ArrayList<>();
+        for (int c: ColorTemplate.VORDIPLOM_COLORS){
+            colors.add(c);
+        }
+
+        for (int c: ColorTemplate.JOYFUL_COLORS){
+            colors.add(c);
+        }
+
+        for (int c: ColorTemplate.COLORFUL_COLORS){
+            colors.add(c);
+        }
+
+        for (int c: ColorTemplate.LIBERTY_COLORS){
+            colors.add(c);
+        }
+
+        for (int c: ColorTemplate.PASTEL_COLORS){
+            colors.add(c);
+        }
+
+        colors.add(ColorTemplate.getHoloBlue());
+
+        pieDataSet.setColors(colors);
+
+        PieData data = new PieData(pieDataSet);
+        data.setValueFormatter(new PercentFormatter());
+        data.setValueTextSize(11f);
+        data.setValueTextColor(Color.GRAY);
+
+        pieChart.setData(data);
+
+        pieChart.highlightValue(null);
+
+        pieChart.invalidate();
+
+
     }
 }
--- WinWin/app/src/main/java/com/tutrieuchau/winwin/MainActivity.java
+++ WinWin/app/src/main/java/com/tutrieuchau/winwin/MainActivity.java
@@ -1,11 +1,15 @@
 package com.tutrieuchau.winwin;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+
+import com.tutrieuchau.winwin.Activity.TimeActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,7 +40,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Intent intent = new Intent(MainActivity.this,TimeActivity.class);
+        startActivity(intent);
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
